bb9d5a5da0ae9f4c77a7e667f6a68609648b5dcf
paridabhhagyashree-svg <paridabhhagyashree-svg>
2026-06-28 15:15:54 +0530

    improve spam detection logic

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Main Flask application for the Fraud Message / Email Detector
 from flask import Flask, render_template, request
 import joblib
 
@@ -7,27 +6,41 @@ app = Flask(__name__)
 model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl")
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
 
     result = ""
+    prediction_type = ""
 
     if request.method == "POST":
 
         message = request.form.get("message")
-        if not message.strip():
-    return render_template("index.html", result="Please enter a message.")
 
-        data = vectorizer.transform([message])
+        if not message or message.strip() == "":
+            result = "⚠ Please enter a message."
+            prediction_type = "warning"
 
-        prediction = model.predict(data)
-
-        if prediction[0] == 1:
-            result = "Spam Message"
         else:
-            result = "Not Spam Message"
 
-    return render_template("index.html", result=result)
+            data = vectorizer.transform([message])
+
+            prediction = model.predict(data)
+
+            if prediction[0] == 1:
+                result = "🚨 Spam Message"
+                prediction_type = "spam"
+
+            else:
+                result = "✅ Not Spam Message"
+                prediction_type = "safe"
+
+    return render_template(
+        "index.html",
+        result=result,
+        prediction_type=prediction_type
+    )
+
 
 if __name__ == "__main__":
     app.run(debug=True)
